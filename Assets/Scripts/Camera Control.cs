159c0b6c4b3862880f63948450a1d3c1cbd338eb
tobysha <892715964@qq.com>
2024-02-04 19:36:26 +0000

    V 0.2.1 camera fixed  and more object
    create a new tower with no script and a monster with partly completed script

--- Assets/Scripts/Camera Control.cs
+++ Assets/Scripts/Camera Control.cs
@@ -7,10 +7,17 @@ using UnityEngine.InputSystem;
 public class CameraControl : MonoBehaviour
 {
     private Vector2 moveValue;
-    private Vector3 TempPosition;
+    //private Vector3 TempPosition;
     private float movespeed = 0.1f;
-    private float smoothTime = 0.3f;
-    private Vector3 smoothVelocity = new Vector3(1f,1f,1f);
+    private float smoothTime = 0.2f;
+    private Vector3 smoothVelocity
+        = new Vector3(1f,1f,1f);
+
+    [SerializeField] private float mouseSensitivity = 3.0f;
+    private float rotationY;
+    private float rotationX;
+    [SerializeField] private Vector2 rotationXMinMax = new Vector2(0, 90);
+    private Vector3 currentRotation;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,7 +25,7 @@ public class CameraControl : MonoBehaviour
     }
     private void initial_set()
     {
-        TempPosition = transform.localPosition;
+        //TempPosition = transform.localPosition;
         
     }
     // Update is called once per frame
@@ -31,20 +38,25 @@ public class CameraControl : MonoBehaviour
     {
         moveValue = value.Get<Vector2>();
         
-        
-        Debug.Log(TempPosition);
+        //Debug.Log(TempPosition);
     }
     void Camera_moving()
     {
-        TempPosition = new Vector3(TempPosition.x + moveValue.x* movespeed, TempPosition.y, TempPosition.z + moveValue.y* movespeed);
+        if(Input.GetKey(KeyCode.Space))
+        {
+            transform.position += new Vector3(0f, movespeed * 1f, 0f);
+        }
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            transform.position -= new Vector3(0f, movespeed * 1f, 0f);
+        }
+        //TempPosition = new Vector3(TempPosition.x + moveValue.x* movespeed, TempPosition.y, TempPosition.z + moveValue.y* movespeed);
         //this.transform.position = TempPosition + (Vector3)moveValue * movespeed;
-        transform.localPosition = Vector3.SmoothDamp(transform.position, TempPosition, ref smoothVelocity, smoothTime);
+        //transform.localPosition = Vector3.SmoothDamp(transform.position, TempPosition, ref smoothVelocity, smoothTime);
+
+        transform.position += transform.forward * moveValue.y * movespeed + transform.right * moveValue.x* movespeed;
     }
-    [SerializeField] private float mouseSensitivity = 3.0f;
-    private float rotationY;
-    private float rotationX;
-    [SerializeField] private Vector2 rotationXMinMax = new Vector2(-80, 20);
-    private Vector3 currentRotation;
+
     private void CameraRotator(bool start)
     {
         if (Input.GetMouseButton(1) || start)
@@ -55,7 +67,7 @@ public class CameraControl : MonoBehaviour
             rotationX += mouseY;
 
             rotationX = Mathf.Clamp(rotationX, rotationXMinMax.x, rotationXMinMax.y);
-            Vector3 player_nextRotation = new Vector3(0f, rotationY);
+            //Vector3 player_nextRotation = new Vector3(0f, rotationY);
 
             Vector3 nextRotation = new Vector3(rotationX, rotationY);
 
@@ -63,7 +75,7 @@ public class CameraControl : MonoBehaviour
             transform.localEulerAngles = currentRotation;
             //transform.localEulerAngles = player_nextRotation;
             ////transform.position = Vector3.SmoothDamp(transform.position, player.transform.position - transform.forward * distance, ref smoothVelocity, smoothTime);
-            transform.position = TempPosition;
+            //transform.position = TempPosition;
             //offset = transform.position - player.transform.position;
             //offset = offset.normalized * freeDistance * proportion;
         }
